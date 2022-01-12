4aa163859d787c2abcd7811120a9f7be1294f2fa
chendongsheng <sexluna@outlook.com>
2022-01-12 18:12:44 +0800

    del build fodler

--- build/CMakeFiles/LogTraslate.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for LogTraslate.
-# This may be replaced when dependencies are built.
--- build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/chendongsheng/github/log_tra
-# Build directory: /Users/chendongsheng/github/log_tra/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("lib/spdlog")
-subdirs("lib/json")
