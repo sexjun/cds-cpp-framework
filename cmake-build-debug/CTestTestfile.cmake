9efa1936ba4f3447949465f0f035a2f2df179b48
chendongsheng <sexluna@outlook.com>
2022-01-13 21:29:31 +0800

    json:parese json file suceess
    Signed-off-by: chendongsheng <sexluna@outlook.com>

---
+++ cmake-build-debug/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /Users/chendongsheng/github/log_tra
+# Build directory: /Users/chendongsheng/github/log_tra/cmake-build-debug
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("lib/spdlog")
+subdirs("lib/json")
